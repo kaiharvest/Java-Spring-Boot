24e23ae043651847cb5e2d807d8892899a357f30
kaiharvest <indradwiprabowo27@gmail.com>
2026-03-15 10:58:50 +0700

    Scope

---
+++ src/test/java/com/indra/belajar_spring_dasar/ScopeTest.java
@@ -0,0 +1,22 @@
+package com.indra.belajar_spring_dasar;
+
+import com.indra.belajar_spring_dasar.data.Foo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ScopeTest {
+
+    @Test
+    void testPrototypeScope() {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ScopeConfiguration.class);
+        Foo foo1 = applicationContext.getBean(Foo.class);
+        Foo foo2 = applicationContext.getBean(Foo.class);
+        Foo foo3 = applicationContext.getBean(Foo.class);
+
+        Assertions.assertNotSame(foo1, foo2);
+        Assertions.assertNotSame(foo1, foo3);
+        Assertions.assertNotSame(foo2, foo3);
+    }
+}
