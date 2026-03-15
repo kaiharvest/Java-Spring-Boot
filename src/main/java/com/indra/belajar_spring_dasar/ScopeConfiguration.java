6bd2e970641caf51f173172d2df0b6b91ede2899
kaiharvest <indradwiprabowo27@gmail.com>
2026-03-15 19:07:45 +0700

    Create Scope

--- src/main/java/com/indra/belajar_spring_dasar/ScopeConfiguration.java
+++ src/main/java/com/indra/belajar_spring_dasar/ScopeConfiguration.java
@@ -1,7 +1,10 @@
 package com.indra.belajar_spring_dasar;
 
+import com.indra.belajar_spring_dasar.data.Bar;
 import com.indra.belajar_spring_dasar.data.Foo;
+import com.indra.belajar_spring_dasar.scope.DoubletonScope;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -15,6 +18,20 @@ public class ScopeConfiguration {
     public Foo foo() {
         log.info("Create new Foo");
         return new Foo();
+    }
+
+    @Bean
+    public CustomScopeConfigurer customScopeConfigurer() {
+        CustomScopeConfigurer customScopeConfigurer = new CustomScopeConfigurer();
+        customScopeConfigurer.addScope("doubleton", new DoubletonScope());
+        return customScopeConfigurer;
+    }
+
+    @Bean
+    @Scope("doubleton")
+    public Bar bar() {
+        log.info("Create new Bar");
+        return new Bar();
     }
 
 }
--- src/test/java/com/indra/belajar_spring_dasar/ScopeTest.java
+++ src/test/java/com/indra/belajar_spring_dasar/ScopeTest.java
@@ -1,5 +1,6 @@
 package com.indra.belajar_spring_dasar;
 
+import com.indra.belajar_spring_dasar.data.Bar;
 import com.indra.belajar_spring_dasar.data.Foo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -18,5 +19,20 @@ public class ScopeTest {
         Assertions.assertNotSame(foo1, foo2);
         Assertions.assertNotSame(foo1, foo3);
         Assertions.assertNotSame(foo2, foo3);
+    }
+
+    @Test
+    void customPrototypeScope() {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ScopeConfiguration.class);
+        Bar bar1 = applicationContext.getBean(Bar.class);
+        Bar bar2 = applicationContext.getBean(Bar.class);
+        Bar bar3 = applicationContext.getBean(Bar.class);
+        Bar bar4 = applicationContext.getBean(Bar.class);
+
+        Assertions.assertNotSame(bar1, bar2);
+        Assertions.assertNotSame(bar3, bar4);
+
+        Assertions.assertSame(bar1, bar3);
+        Assertions.assertSame(bar2, bar4);
     }
 }
