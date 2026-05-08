3b67594896d22432726c46e8507e07673ebc87cc
kaiharvest <indradwiprabowo27@gmail.com>
2026-05-08 09:22:51 +0700

    Event Listener

--- src/test/java/com/indra/belajar_spring_dasar/EventListenerTest.java
+++ src/test/java/com/indra/belajar_spring_dasar/EventListenerTest.java
@@ -1,7 +1,12 @@
 package com.indra.belajar_spring_dasar;
 
+import com.indra.belajar_spring_dasar.listener.LoginAgainSuccessListener;
 import com.indra.belajar_spring_dasar.listener.LoginSuccessListener;
 import com.indra.belajar_spring_dasar.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -10,10 +15,26 @@ public class EventListenerTest {
     @Configuration
     @Import({
             UserService.class,
-            LoginSuccessListener.class
+            LoginSuccessListener.class,
+            LoginAgainSuccessListener.class
     })
     public static class TestConfiguration {
 
     }
 
+    private ConfigurableApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(TestConfiguration.class);
+        applicationContext.registerShutdownHook();
+    }
+
+    @Test
+    void testEven() {
+        UserService userService = applicationContext.getBean(UserService.class);
+        userService.login("indra", "indra");
+        userService.login("indra", "salah");
+        userService.login("dwi", "salah");
+    }
 }
